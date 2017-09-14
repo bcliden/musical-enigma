325c5a0181bf6c07f3cd5b452acc1cb940b643d0
Benjamin Liden <benjamin.c.liden@gmail.com>
2017-09-13 22:50:21 -0400

    "Edit" and "Update" routes working. Index page style & buttons on "Show" page tweaked. Sanitized textareas

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var 	express 	= require("express"),
-	app 		= express(),
-	bodyParser 	= require("body-parser"),
-	mongoose 	= require("mongoose");
+var 	express 			= require("express"),
+			app 					= express(),
+			bodyParser 		= require("body-parser"),
+			mongoose 			= require("mongoose"),
+			methodOverride = require("method-override"),
+			expressSanitizer = require("express-sanitizer");
 
 //APP CONFIG
 mongoose.connect("mongodb://localhost/restfulblog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 // MONGOOSE / MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -44,6 +47,7 @@ app.get("/blogs/new", function(req, res){
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
 	//create blog
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			res.render("new");
@@ -62,7 +66,42 @@ app.get("/blogs/:id", function(req, res){
 		} else {
 			res.render("show", {blog: foundBlog});
 		};
-	})
+	});
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	 Blog.findById(req.params.id, function(err, foundBlog){
+		 if(err){
+			 res.redirect("/blogs");
+		 } else {
+			 res.render("edit", {blog: foundBlog});
+	 	};
+	});
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res,redirect("/blogs/" + req.params.id);
+		};
+	});
+});
+
+//DESTROY ROUTE
+app.delete("/blogs/:id", function(req, res){
+//destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+		res.redirect("/blogs");
+		} else {
+		res.redirect("/blogs")
+		};
+	});
 });
 
 //SERVE
